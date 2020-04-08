32190e65c84386967fe6167edfcef3fab689b1c1
pratikunterwegs <pratikgupte16@gmail.com>
2020-04-08 14:32:07 +0200

    add more summary functions

--- R/fun_global_summary.r
+++ R/fun_global_summary.r
@@ -2,13 +2,17 @@
 #'
 #' @param data_path Where to search for data.
 #' @param type The type of simulation to search for.
+#' @param which_summary Which summary to create. May be "all", "parameter_summary", "parameter_counts", "phenotype_counts".
 #'
 #' @return Prints figures and saves data providing a global summary of all data in the data folder.
 #' @import data.table
 #' @export
 print_global_summary <- function(data_path = "data",
-                                 type = "info"){
-  id <- value <- round_value <- gen <- variable <- parameter <- summary_data <- NULL
+                                 type = "info",
+                                 which_summary = "all"){
+  id <- value <- round_value <- gen <- NULL
+  variable <- parameter <- summary_data <- NULL
+  a <- b <- NULL
   # asserts for file path
   {
     # check the data path is in the current working directory
@@ -17,8 +21,8 @@ print_global_summary <- function(data_path = "data",
 
     # list agent data folders
     data_folders <- list.dirs(path = data_path,
-                            recursive = F,
-                            full.names = TRUE)
+                              recursive = F,
+                              full.names = TRUE)
 
     # check if type is not ALL and subset
     {
@@ -29,35 +33,36 @@ print_global_summary <- function(data_path = "data",
       # check that these folders actually exist
       purrr::walk(data_folders, function(fol){
         assertthat::assert_that(dir.exists(fol),
-        msg = glue::glue('sim type {stringr::str_remove(fol, "data/")} does not exist'))
+        msg = glue::glue('sim type {stringr::str_remove(fol, "data/")}
+                         does not exist'))
       })
     }
 
     # check that there is some data
     assertthat::assert_that(length(data_folders) >= 1,
-                          msg = "global_summary: no output folders in data path")
+    msg = "global_summary: no output folders in data path")
   }
 
   # ignore folders where there is no data
   {
     data_folders <- purrr::keep(data_folders, function(fol){
       n_files <- length(list.files(path = glue::glue('{fol}/agent_summary'),
-                            pattern = ".csv"))
+                                   pattern = ".csv"))
       return(n_files >= 1)
     })
   }
 
   # walk over the type folders
-  purrr::walk(data_folders, function(fol){
+  purrr::walk(data_folders, function(fol)
+  {
     # acquire a lookup file
     lookup <- list.files(path = fol,
                          pattern = "lookup",
                          full.names = TRUE)
     # check that there is some data
     assertthat::assert_that(length(lookup) == 1,
-                            msg = "global_summary: no or multiple lookups found")
+            msg = "global_summary: no or multiple lookups found")
     lookup <- data.table::fread(lookup)
-
     # list the data files
     data_files <- list.files(path = glue::glue('{fol}/agent_summary'),
                              pattern = ".csv")
@@ -76,58 +81,100 @@ print_global_summary <- function(data_path = "data",
                                 msg = glue::glue('global_summary: {namesReq[i]} is
                          required but missing from data'))
       }
-    }
-    # read in data
-    {
       # read the data and summarise
       agent_data <- purrr::map(glue::glue('{fol}/agent_summary/{data$filename}'),
                                data.table::fread)
-      agent_summary <- purrr::map(agent_data, function(dt){
-        dt[,id:=NULL]
+    }
 
-        # convert all cols to numeric
-        dt <- dt[,lapply(.SD, as.numeric)]
-        dt <- data.table::melt(dt, id.vars = "gen", variable.name="parameter")
-        dt <- dt[,.(mean = mean(value),
-              sd = stats::sd(value)),
-           by = .(gen, parameter)]
-      })
+    #### global generation specific parameter means ####
+    if(which_summary %in% c("parameter_summary", "all"))
+    {
+      # read in data
+      {
+        tmp_summary <- purrr::map(agent_data, function(dt){
+          dt[,id:=NULL]
 
-      # remove the filename
+          # convert all cols to numeric
+          dt <- dt[,lapply(.SD, as.numeric)]
+          dt <- data.table::melt(dt, id.vars = "gen", variable.name="parameter")
+          dt <- dt[,.(mean = mean(value),
+                      sd = stats::sd(value)),
+                   by = .(gen, parameter)]
+        })
+
+        # remove the filename
+        tmp_data <- data
+        tmp_data[,`:=`(filename=NULL,
+                       summary_data = tmp_summary)]
+
+        # unlist the list column
+        tmp_data <- tmp_data[, unlist(summary_data, recursive = FALSE),
+                             by = setdiff(names(tmp_data), "summary_data")]
+      }
+      # save summary to file
+      {
+        data.table::fwrite(x = tmp_data, file = glue::glue('{fol}/data_global_summary.csv'))
+      }
+    }
+
+    #### global generation and parameter specific counts ####
+    if(which_summary %in% c("parameter_counts", "all"))
+    {
+      # get proportion data for plots
+      {
+        tmp_summary <- purrr::map(agent_data, function(dt){
+          # convert all cols to numeric
+          dt <- dt[,lapply(.SD, as.numeric)]
+          dt <- data.table::melt(dt, id.vars = "gen")
+          dt[,round_value := round(value, 1)]
+
+          dt[,.(count = .N),
+             by = .(gen, variable, round_value)]
+        })
+      }
+      # add to parameter data
       tmp_data <- data
-      tmp_data[,`:=`(filename=NULL,
-                     summary_data = agent_summary)]
+      tmp_data[,`:=`(summary_data = tmp_summary)]
 
       # unlist the list column
-      tmp_data <- tmp_data[, unlist(summary_data, recursive = FALSE),
+      tmp_data <- tmp_data[, unlist(tmp_data, recursive = FALSE),
                            by = setdiff(names(tmp_data), "summary_data")]
+      # save summary to file
+      {
+        data.table::fwrite(x = tmp_data, file = glue::glue('{fol}/data_global_counts.csv'))
+      }
     }
-    # save summary to file
-    {
-      data.table::fwrite(x = tmp_data, file = glue::glue('{fol}/data_global_summary.csv'))
-    }
-    # get proportion data for plots
-    {
-      agent_summary <- purrr::map(agent_data, function(dt){
-        # convert all cols to numeric
-        dt <- dt[,lapply(.SD, as.numeric)]
-        dt <- data.table::melt(dt, id.vars = "gen")
-        dt[,round_value := round(value, 1)]
 
-        dt[,.(count = .N),
-           by = .(gen, variable, round_value)]
-      })
-    }
-    # add to parameter data
-    tmp_data <- data
-    tmp_data[,`:=`(summary_data = agent_summary)]
-
-    # unlist the list column
-    tmp_data <- tmp_data[, unlist(summary_data, recursive = FALSE),
-                         by = setdiff(names(tmp_data), "summary_data")]
-    # save summary to file
+    #### global parameter combination fitness, counts, and following ####
+    if(which_summary %in% c("phenotype_counts", "all"))
     {
-      data.table::fwrite(x = tmp_data, file = glue::glue('{fol}/data_global_counts.csv'))
+      # get genotype counts and mean phenotype
+      {
+        tmp_summary <- purrr::map(agent_data, function(dt){
+          # convert all cols to numeric
+          dt <- dt[,lapply(.SD, as.numeric)]
+          # get rounded values of parameters a and b
+          dt[,`:=`(a = round(a, 1),
+                   b = round(b, 1))]
+          dt <- dt[,.(mean_f = mean(`F`),
+                      sd_f = stats::sd(`F`),
+                      count = .N),
+                   by = .(gen, a, b)]
+        })
+
+      }
+      # add to parameter data
+      tmp_data <- data
+      tmp_data[, `:=`(summary_data = tmp_summary)]
+
+      # unlist list column
+      tmp_data <- tmp_data[, unlist(tmp_data, recursive = FALSE),
+                           by = setdiff(names(tmp_data), "summary_data")]
+      # save summary to file
+      {
+        data.table::fwrite(x = tmp_data,
+                           file = glue::glue('{fol}/data_phenotype_counts.csv'))
+      }
     }
   })
 }
